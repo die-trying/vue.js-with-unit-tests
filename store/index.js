2a9d6f3f170f21926ba8a05165656652ddfc3a08
Fedor Borshev <fedor@borshev.com>
2021-04-20 21:56:36 +0300

    More compliated menu format

--- store/index.js
+++ store/index.js
@@ -2,7 +2,7 @@ export default {
   state: () => {},
   actions: {
     async nuxtServerInit({ commit }, { app }) {
-      const menu = await app.$axios.$get('categories_tree/');
+      const menu = await app.$axios.$get('https://borshev.com/die-trying-menu.json');
       commit('the_menu/SET_MENU', menu);
     },
   },
